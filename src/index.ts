ae3a960244127b2290b592f8934c8924947e6799
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-08-04 00:13:00 -0300

    config: concluded config pre-commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => console.log(`listening on port ${port}`));
